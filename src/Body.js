f9094f37224964704dc91441b0ea96eebdb64c81
Spacenin <chepstudios@gmail.com>
2024-01-27 22:59:34 -0500

    idk

---
+++ src/Body.js
@@ -0,0 +1,11 @@
+import "./index.css"
+
+export default function Body() {
+    return(
+        <div className>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed turpis tincidunt id aliquet risus feugiat in. Sed sed risus pretium quam. Orci dapibus ultrices in iaculis nunc sed. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Placerat orci nulla pellentesque dignissim enim sit amet. Nisl condimentum id venenatis a. At auctor urna nunc id cursus metus aliquam eleifend. Massa sed elementum tempus egestas sed sed risus pretium quam. Odio ut enim blandit volutpat. Diam volutpat commodo sed egestas. Tempor orci eu lobortis elementum nibh tellus molestie nunc. Eget lorem dolor sed viverra ipsum nunc aliquet. Faucibus interdum posuere lorem ipsum dolor sit. Tincidunt arcu non sodales neque. Cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo. Est lorem ipsum dolor sit amet. Dolor sit amet consectetur adipiscing elit. Urna et pharetra pharetra massa massa ultricies mi quis.
+            </p>
+        </div>
+    );
+}
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import "./index.css"
+
+export default function Header() {
+    return(
+        <header>
+            <nav className>
+                <a href="#">Hello</a>
+                <a href="#">World!</a>
+            </nav>
+        </header>
+    );
+}
