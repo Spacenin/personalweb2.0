866b6d4313452e9f96af3ccb1d555454c6e38e77
Spacenin <chepstudios@gmail.com>
2024-01-31 16:08:41 -0500

    working on navbar looks

--- README.md
+++ README.md
@@ -2,8 +2,5 @@
 Personal webpage to bring together my useful links and portfolio of projects. 
 
 --Credits--  
-[Favicon Icon](https://www.flaticon.com/free-icon/cpu_1383401?term=cpu%20tower&page=1&position=28&page=1&position=28&related_id=1383401&origin=search)  
-[Github Icon](https://www.flaticon.com/premium-icon/github_4926624?term=github&page=1&position=13&page=1&position=13&related_id=4926624&origin=search)  
-[LinkedIn Icon](https://www.flaticon.com/premium-icon/linkedin_3128219?term=linkedin&page=1&position=4&page=1&position=4&related_id=3128219&origin=search)  
-[Clemson Icon](https://www.flaticon.com/free-icon/mortarboard_1940611?term=education&page=1&position=4&page=1&position=4&related_id=1940611&origin=tag)  
-[Resume Icon](https://www.flaticon.com/premium-icon/file_2822526?term=document&page=1&position=10&page=1&position=10&related_id=2822526&origin=search)  
+[Favicon Icon](https://www.flaticon.com/free-icon/cpu_1383401?term=cpu%20tower&page=1&position=28&page=1&position=28&related_id=1383401&origin=search)
+[Resume Icon](https://www.flaticon.com/free-icon/document_9720850?term=document&related_id=9720850)
--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import "./index.css"
+// import pdf from "./resume.pdf"
 
 //Header that displays navbar and other information at top of screen, like a header do
 export default function Header() {
@@ -7,10 +8,22 @@ export default function Header() {
             <nav className="inline-flex w-full justify-end">
                 <a className="flex p-4 mr-auto font-bold text-2xl items-center">Charles E. Phillips</a>
                 <div className="flex font-semibold">
-                    <a className="flex rounded py-4 px-8 hover:bg-custom-hover-nav items-center" href="https://github.com/Spacenin" target="_blank">Github</a>
-                    <a className="flex rounded py-4 px-8 hover:bg-custom-hover-nav items-center" href="https://clemson-csm.symplicity.com/profiles/elliot.phillips" target="_blank">Clemson Jobs</a>
-                    <a className="flex rounded py-4 px-8 hover:bg-custom-hover-nav items-center" href="https://www.linkedin.com/in/elliophill/" target="_blank">LinkedIn</a>
-                    <a className="flex rounded py-4 px-8 hover:bg-custom-hover-nav items-center" href="#" target="_blank">Resume</a>
+                    <a className="flex rounded py-4 px-8 hover:bg-custom-hover-nav items-center" href="https://github.com/Spacenin" target="_blank">
+                        Github
+                        <img className="h-7 mx-2" src={process.env.PUBLIC_URL + "/github.png"}></img>
+                    </a>
+                    <a className="flex rounded py-4 px-8 hover:bg-custom-hover-nav items-center" href="https://clemson-csm.symplicity.com/profiles/elliot.phillips" target="_blank">
+                        Clemson Jobs
+                        <img className="h-7 mx-2" src={process.env.PUBLIC_URL + "/paw.png"}></img>
+                    </a>
+                    <a className="flex rounded py-4 px-8 hover:bg-custom-hover-nav items-center" href="https://www.linkedin.com/in/elliophill/" target="_blank">
+                        LinkedIn
+                        <img className="h-7 mx-2" src={process.env.PUBLIC_URL + "/linkedin.png"}></img> 
+                    </a>
+                    <a className="flex rounded py-4 px-8 hover:bg-custom-hover-nav items-center" href={process.env.PUBLIC_URL + "/resume.pdf"} target="_blank">
+                        Resume
+                        <img className="h-7 mx-2" src={process.env.PUBLIC_URL + "/document.png"}></img>
+                    </a>
                 </div>
             </nav>
         </header>
